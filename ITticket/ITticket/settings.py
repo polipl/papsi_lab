7f60d8bbd82f1f93789d454017e771859a1909c4
Pawel Malecki <pm@umx.pl>
2022-11-11 02:02:28 +0100

    added gunicorn nginx proxy

--- ITticket/ITticket/settings.py
+++ ITticket/ITticket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#_s$b7*_wa-ov35ap7)ctqz%k)fk6dv^)))1^-)(_y6j*+x6y_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,8 +123,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_ROOT = "/ITticket/static"
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+MEDIA_ROOT = '/ITticket/media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
