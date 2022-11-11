5c7fbe5a10b7d37ac78410fce04ec803f3599537
Pawel Malecki <pm@umx.pl>
2022-11-11 10:54:59 +0100

    add nav bar

--- ITticket/MAINPAGE/views.py
+++ ITticket/MAINPAGE/views.py
@@ -5,7 +5,10 @@ from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 
 class MainPage(View):
-    """ klasa zwraca widok index.html głównej strony
+    """ 
+        klasa zwraca widok index.html głównej strony
+
+        :template:`MAINPAGE/index.html`
     """
     def get(self, request, *args, **kwargs):
         return render(request,'MAINPAGE/index.html')
