373bcfedd022596d1d984747d0d3f9fc55dcada8
Pawel Malecki <pm@umx.pl>
2022-11-11 16:47:36 +0100

    added SERVICEDESK module

--- ITticket/DICTIONARY/admin.py
+++ ITticket/DICTIONARY/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from .models import TicketTypes
+from .models import TicketTypes, TicketsPriority, TicketsStatus
 # Register your models here.
 
 class TicketTypesAdmin(admin.ModelAdmin):
     list_display = [f.name for f in TicketTypes._meta.fields]
 
 admin.site.register(TicketTypes,TicketTypesAdmin)
+
+class TicketsPriorityAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in TicketsPriority._meta.fields]
+
+admin.site.register(TicketsPriority,TicketsPriorityAdmin)
+
+
+class TicketsStatusAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in TicketsStatus._meta.fields]
+
+admin.site.register(TicketsStatus,TicketsStatusAdmin)
--- ITticket/DICTIONARY/models.py
+++ ITticket/DICTIONARY/models.py
@@ -1,12 +1,45 @@
+from datetime import datetime
 from django.db import models
-
+from django.contrib.auth.models import User,Group
 # Create your models here.
+
 class TicketTypes(models.Model):
-    type = models.CharField(max_length=20,null=True,blank=True)
+    """
+    Model słownik - definicja typów ticketów
+    """
+    type_name = models.CharField(max_length=20,null=True,blank=True)
    
     def __str__(sefl):
-        return '%s' % (sefl.type)
+        return '%s' % (sefl.type_name)
 
     class Meta:
         verbose_name = 'Typ Ticketu'
         verbose_name_plural = 'Typy Ticketów'
+
+class TicketsPriority(models.Model):
+    """
+    Model słownik - definicja priorytetów ticketów
+    """
+    priority_name = models.CharField(max_length=20,null=True,blank=True)
+   
+    def __str__(sefl):
+        return '%s' % (sefl.priority_name)
+
+    class Meta:
+        verbose_name = 'Priorytet Ticketu'
+        verbose_name_plural = 'Priorytety Ticketów'
+
+class TicketsStatus(models.Model):
+    """
+    Model słownik - definicja statusów ticketów
+    """
+    status_name = models.CharField(max_length=40,null=True,blank=True)
+   
+    def __str__(sefl):
+        return '%s' % (sefl.status_name)
+
+    class Meta:
+        verbose_name = 'Status Ticketu'
+        verbose_name_plural = 'Statusy Ticketów'
+
+
