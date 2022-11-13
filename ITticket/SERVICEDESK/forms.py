c88488894ad864130c6206054f05439ff892a5fc
Pawel Malecki <pm@umx.pl>
2022-11-13 01:22:59 +0100

    add acl conditions redirect for USERS group and IT group

--- ITticket/SERVICEDESK/forms.py
+++ ITticket/SERVICEDESK/forms.py
@@ -15,6 +15,16 @@ class CreateTicketForm(forms.models.ModelForm):
         model = Tickets
         fields = ['created_user','short_description','description','assigned_user','ticket_type','priority']
 
+class CreateTicketEndUserForm(forms.models.ModelForm):
+    short_description = forms.CharField(label=False,widget=forms.TextInput(attrs={'class': 'form-control my-1', 'placeholder': 'Krótki opis problemu'}))
+    description = forms.CharField(label=False, widget=forms.Textarea(attrs={'class': 'form-control my-1', 'placeholder': 'Opis problemu'}))
+    priority = forms.ModelChoiceField(label='Priorytet',queryset=TicketsPriority.objects.all(),widget=forms.Select(attrs={'class': 'form-control my-1', 'placeholder': 'Priorytet'}),empty_label=None)
+    ticket_type = forms.ModelChoiceField(label='Typ zgłoszenia',queryset=TicketTypes.objects.all(),widget=forms.Select(attrs={'class': 'form-control my-1', 'placeholder': 'Typ zgłoszenia'}),empty_label=None)
+    class Meta:
+        model = Tickets
+        fields = ['short_description','description','ticket_type','priority']
+
+
 class EditTicketForm(forms.models.ModelForm):
     created_user = forms.ModelChoiceField(label='Zgłaszający',required=True,queryset=User.objects.all(),widget=forms.Select(attrs={'class': 'form-control my-1', 'placeholder': 'Zgłaszający'}),empty_label=None)
     short_description = forms.CharField(label=False,widget=forms.TextInput(attrs={'class': 'form-control my-1', 'placeholder': 'Krótki opis problemu'}))
@@ -26,6 +36,15 @@ class EditTicketForm(forms.models.ModelForm):
     class Meta:
         model = Tickets
         fields = ['created_user','short_description','description','assigned_user','ticket_type','priority','status']
+
+class EditTicketEndUserForm(forms.models.ModelForm):
+    short_description = forms.CharField(label=False,widget=forms.TextInput(attrs={'class': 'form-control my-1', 'placeholder': 'Krótki opis problemu'}))
+    description = forms.CharField(label=False, widget=forms.Textarea(attrs={'class': 'form-control my-1', 'placeholder': 'Opis problemu'}))
+    priority = forms.ModelChoiceField(label='Priorytet',queryset=TicketsPriority.objects.all(),widget=forms.Select(attrs={'class': 'form-control my-1', 'placeholder': 'Priorytet'}),empty_label=None)
+    ticket_type = forms.ModelChoiceField(label='Typ zgłoszenia',queryset=TicketTypes.objects.all(),widget=forms.Select(attrs={'class': 'form-control my-1', 'placeholder': 'Typ zgłoszenia'}),empty_label=None)
+    class Meta:
+        model = Tickets
+        fields = ['short_description','description','ticket_type','priority']
 
 
 class CreateTicketHistoryForm(forms.models.ModelForm):
--- ITticket/SERVICEDESK/views.py
+++ ITticket/SERVICEDESK/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 
 from DICTIONARY.models import TicketsStatus
 from .models import Tickets, TicketsHistory
-from .forms import CreateTicketForm, CreateTicketHistoryForm, EditTicketForm
+from .forms import CreateTicketForm, CreateTicketHistoryForm, EditTicketForm, CreateTicketEndUserForm, EditTicketEndUserForm
 # Create your views here.
 
 @method_decorator(login_required, name='dispatch')
@@ -30,6 +30,8 @@ class TicketsList(View):
     :template:`SERVICEDESK/tickets_list.html`
     """
     def get(self, request, *args, **kwargs):
+        if request.user.groups.filter(name="USERS"):
+            return redirect('SERVICEDESK:ticket_created_by_user_list') 
         tickets = Tickets.objects.all().order_by('-pk')
         context = { 'tickets': tickets }
         return render(request, 'SERVICEDESK/tickets_list.html', context)
@@ -42,7 +44,21 @@ class TicketsAssignedList(View):
     :template:`SERVICEDESK/tickets_list.html`
     """
     def get(self, request, *args, **kwargs):
+        if request.user.groups.filter(name="USERS"):
+            return redirect('SERVICEDESK:ticket_created_by_user_list') 
         tickets = Tickets.objects.filter(assigned_user=request.user).order_by('-pk')
+        context = { 'tickets': tickets }
+        return render(request, 'SERVICEDESK/tickets_list.html', context)
+
+@method_decorator(login_required, name='dispatch')
+class TicketsCreatedByUserList(View):
+    """
+    Widok wyświetla wszystkie tickety utworzone do zalogowanego użytkownika
+
+    :template:`SERVICEDESK/tickets_list.html`
+    """
+    def get(self, request, *args, **kwargs):
+        tickets = Tickets.objects.filter(created_user=request.user).order_by('-pk')
         context = { 'tickets': tickets }
         return render(request, 'SERVICEDESK/tickets_list.html', context)
 
@@ -87,17 +103,44 @@ class CreateTicket(View):
     :template:SERVICEDESK/create_ticket.html
     """
     def get(self, request, *args, **kwargs):
+        if request.user.groups.filter(name="USERS"):
+            return redirect('SERVICEDESK:ticket_create_by_user') 
         form = CreateTicketForm()
         context = { 'form' : form}
         return render(request, 'SERVICEDESK/create_ticket.html', context)
 
     def post(self, request, *args, **kwargs):
+        if request.user.groups.filter(name="USERS"):
+            return redirect('SERVICEDESK:ticket_create_by_user') 
         form = CreateTicketForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.status = TicketsStatus.objects.get(pk=1)
             post.save()
         return redirect('SERVICEDESK:ticket_list')
+
+@method_decorator(login_required, name='dispatch')
+class CreateTicketByUser(View):
+    """
+    Widok - tworzy nowy ticket przez użytkownika końcowego
+
+    :template:SERVICEDESK/create_ticket.html
+    """
+    def get(self, request, *args, **kwargs):
+        form = CreateTicketEndUserForm()
+        context = { 'form' : form }
+        return render(request, 'SERVICEDESK/create_ticket.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = CreateTicketEndUserForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.status = TicketsStatus.objects.get(pk=1)
+            post.created_user = request.user
+            post.save()
+        return redirect('SERVICEDESK:ticket_created_by_user_list')
+
+
 
 @method_decorator(login_required, name='dispatch')
 class EditTicket(View):
@@ -108,15 +151,39 @@ class EditTicket(View):
     """
     def get(self, request, ticket_id, *args, **kwargs):
         ticket = get_object_or_404(Tickets,pk=ticket_id)
+        if request.user.groups.filter(name="USERS"):
+            return redirect('SERVICEDESK:ticket_edit_by_user') 
         form = EditTicketForm(instance=ticket)
         context = { 'form' : form}
         return render(request, 'SERVICEDESK/edit_ticket.html', context)
 
     def post(self, request, ticket_id, *args, **kwargs):
         ticket = get_object_or_404(Tickets,pk=ticket_id)
+        if request.user.groups.filter(name="USERS"):
+            return redirect('SERVICEDESK:ticket_edit_by_user') 
         form = EditTicketForm(request.POST, instance=ticket)
         if form.is_valid():
             post = form.save(commit=False)
             post.save()
         return redirect('SERVICEDESK:ticket_details',ticket_id)
 
+@method_decorator(login_required, name='dispatch')
+class EditTicketByUser(View):
+    """
+    Widok - Edytuje nowy ticket przez użytkownika końcowego
+
+    :template:SERVICEDESK/edit_ticket.html
+    """
+    def get(self, request, ticket_id, *args, **kwargs):
+        ticket = get_object_or_404(Tickets,pk=ticket_id)
+        form = EditTicketEndUserForm(instance=ticket)
+        context = { 'form' : form}
+        return render(request, 'SERVICEDESK/edit_ticket.html', context)
+
+    def post(self, request, ticket_id, *args, **kwargs):
+        ticket = get_object_or_404(Tickets,pk=ticket_id)
+        form = EditTicketEndUserForm(request.POST, instance=ticket)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+        return redirect('SERVICEDESK:ticket_details',ticket_id)
