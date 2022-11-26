cb04e144f52c83eb4a00e4603c0cc21a27c1cf4d
Paweł Małecki <pm@umx.pl>
2022-11-26 22:05:10 +0100

    .

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-docker network create itticketnet
-docker run --name mariadb -v $(pwd)/files/mysqldb:/var/lib/mysql -e MARIADB_ROOT_PASSWORD=adminsql123q -p 3306:3306 --network itticketnet -d mariadb:latest
-docker build -t pythdjango -f ./Dockerfile .
-docker run --name itticket -p 8000:8000 --network itticketnet -v $(pwd)/ITticket/:/ITticket pythdjango
-
+#1/bin/sh
+docker-compose up -d
+docker cp files/dump.sql papsi_lab_postgres_1:/
+docker exec papsi_lab_postgres_1 psql -U postgres -f /dump.sql
---
+++ manual-build.sh
@@ -0,0 +1,5 @@
+docker network create itticketnet
+docker run --name mariadb -v $(pwd)/files/mysqldb:/var/lib/mysql -e MARIADB_ROOT_PASSWORD=adminsql123q -p 3306:3306 --network itticketnet -d mariadb:latest
+docker build -t pythdjango -f ./Dockerfile .
+docker run --name itticket -p 8000:8000 --network itticketnet -v $(pwd)/ITticket/:/ITticket pythdjango
+
