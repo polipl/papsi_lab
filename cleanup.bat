b2a0b6736506ca01790c5d0fa8986c6aa449e2f8
Paweł Małecki <pm@umx.pl>
2022-12-29 16:20:51 +0100

    change install scripts

--- cleanup.bat
+++
@@ -1,5 +0,0 @@
-docker compose stop
-docker system prune -a -f
-rmdir "files/postgres" /S /Q
-
-
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+docker compose -f docker-compose-dev.yml up -d
+docker exec -i papsi_lab_postgres_1 psql -U postgres -c "CREATE DATABASE itticket"
+docker exec -i papsi_lab_postgres_1 psql -U postgres -c "CREATE USER itticket WITH PASSWORD 'adminsql123q'"
+docker exec -i papsi_lab_postgres_1 psql itticket -U postgres -c "ALTER ROLE itticket SET client_encoding TO 'utf8'"
+docker exec -i papsi_lab_postgres_1 psql itticket -U postgres -c "ALTER ROLE itticket SET default_transaction_isolation TO 'read committed'"
+docker exec -i papsi_lab_postgres_1 psql itticket -U postgres -c "ALTER ROLE itticket SET timezone TO 'UTC'"
+docker exec -i papsi_lab_postgres_1 psql itticket -U postgres -c "GRANT ALL PRIVILEGES ON SCHEMA public to itticket"
+docker exec -i papsi_lab_postgres_1 psql itticket -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE itticket to itticket"
+docker exec -i papsi_lab_postgres_1 psql itticket -U postgres -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO itticket"
+docker exec -i papsi_lab_postgres_1 psql itticket -U postgres -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO itticket"
+docker cp files/dump-pgsql.sql papsi_lab_postgres_1:/
+docker exec -i papsi_lab_postgres_1 psql -U postgres -f dump-pgsql.sql
+
+
