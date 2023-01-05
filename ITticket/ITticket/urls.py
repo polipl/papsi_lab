42b25c8e152b49914215427923eadd89cc9e290d
Paweł Małecki <pm@umx.pl>
2023-01-06 00:30:28 +0100

    del unused urls

--- ITticket/ITticket/urls.py
+++ ITticket/ITticket/urls.py
@@ -27,17 +27,13 @@ router.register(r"ticket_type", apiview.TicketTypesViewSet)
 router.register(r"tickets_priority", apiview.TicketsPriorityViewSet)
 router.register(r"tickets", apiview.TicketsViewSet)
 
-urlpatterns = (
-    [
-        path("", include("MAINPAGE.urls")),
-        path("dictionary/", include("DICTIONARY.urls", namespace="DICTIONARY")),
-        path("servicedesk/", include("SERVICEDESK.urls", namespace="SERVICEDESK")),
-        path("admin/doc/", include("django.contrib.admindocs.urls")),
-        path("admin/", admin.site.urls),
-        path("accounts/", include("django.contrib.auth.urls")),
-        path("api/v1/", include(router.urls)),
-        path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
-    ]
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-)
+urlpatterns = [
+    path("", include("MAINPAGE.urls")),
+    path("dictionary/", include("DICTIONARY.urls", namespace="DICTIONARY")),
+    path("servicedesk/", include("SERVICEDESK.urls", namespace="SERVICEDESK")),
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("api/v1/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+]
