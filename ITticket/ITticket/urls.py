70a27a82ec4f57c161785beb6eb3de697c9e2f61
Paweł Małecki <pm@umx.pl>
2023-01-06 00:32:20 +0100

    del unused libs

--- ITticket/ITticket/urls.py
+++ ITticket/ITticket/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from SERVICEDESK import apiview
-from django.conf import settings
-from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r"users", apiview.UserViewSet)
